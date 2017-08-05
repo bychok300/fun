c8e417148125108b20f60d50e192c2e924c5e5dd
Anton <bychok300@inbox.ru>
2017-08-05 17:29:32 +0300

    add useful methods

--- cryptMe.py
+++ cryptMe.py
@@ -1,12 +1,15 @@
 from cryptography.fernet import Fernet
 import sys
-
-
+import pyscrypt
+import getpass
 #-----------------------
 #This method will will parse command line arguments
 #----------------------- 
+list_of_args = ['-h','-e', '-d','-g']
 
 def parse_args():
+    
+#     list_of_args = ['-h','-e', '-d','-g']
     
     if len(sys.argv)==1:
         print(
@@ -16,7 +19,7 @@ def parse_args():
                 -g, generate secret key
               ''')
     if len(sys.argv)>1:
-        if sys.argv[1]=='-h':
+        if sys.argv[1]==list_of_args[0]:
             print(
 '''Usage: cryptMe.py -e [encrypt message], -d [decrypt message]'
                 -e, will start script and encrypt message
@@ -24,12 +27,13 @@ def parse_args():
                 -g, generate secret key
 ''')
         
-        if sys.argv[1]=='-e':
+        if sys.argv[1]==list_of_args[1]:
             encrypt()
-        if sys.argv[1]=='-d':
+        if sys.argv[1]==list_of_args[2]:
             decrypt()
-        if sys.argv[1]=='-g':
+        if sys.argv[1]==list_of_args[3]:
             generate_sec_key()
+            
             
         if sys.argv[1] != '-e' and sys.argv[1] != '-d' and sys.argv[1] != '-g' and sys.argv[1] != '-h':
             print(
@@ -45,9 +49,21 @@ def parse_args():
 #This method will generate secret key
 #-----------------------    
 
-def generate_sec_key():                                     
-    key = Fernet.generate_key()                            #generate key using Fernet class         
-    print(str(key)[2:-1])
+def generate_sec_key():  
+                                       
+#     key = Fernet.generate_key()                            #generate key using Fernet class         
+    password = getpass.getpass()                                    
+    hashed = pyscrypt.hash(password = userMesgToBytes(password), 
+                       salt = b"seasalt", 
+                       N = 1024, 
+                       r = 1, 
+                       p = 1, 
+                       dkLen = 32) 
+    if len(sys.argv)>1:
+        if sys.argv[1]==list_of_args[3]:
+            print(str(hashed)[2:-1])
+    return(str(hashed)[2:-1])
+#     print(str(key)[2:-1])
 
 
 #-----------------------
@@ -56,7 +72,7 @@ def generate_sec_key():
 
 
 def encrypt():
-    print('Please set the path to key file : ')
+#     print('Please set the path to key file : ')
     
 #-------------------
 #Fernet guarantees that a message encrypted using it cannot be manipulated or read without the key.
@@ -64,17 +80,17 @@ def encrypt():
 #Fernet also has support for implementing key rotation via MultiFernet.
 #-------------------
 
-    path = input()                                         #user input for path to key               
-    file = open(path,'r',encoding='utf-8')                 #open file with your key
-    key = file.read()                                      #read file 
-    f = Fernet(key)                                        #use fernet     
+#     path = input()                                         #user input for path to key               
+#     file = open(path,'r',encoding='utf-8')                 #open file with your key
+#     key = file.read()                                      #read file 
+    f = Fernet(generate_sec_key())                                        #use fernet     
                                          
     print('Your message :')
     
     token = f.encrypt(userMesgToBytes(input()))            #encrypt message  
     
-    print('Your message have been encrypted :')
-    print(str(token)[2:-1])                                #delete first two and last symbols(b'.....')
+    print('Your message have been encrypted : \n')
+    print(str(token)[2:-1],'\n')                                #delete first two and last symbols(b'.....')
     
 
 #-----------------------
@@ -82,19 +98,19 @@ def encrypt():
 #-----------------------
 
 def decrypt():
-    print('Please set the path to friend key file : ')
-    
-    path = input()                                         #user input for path to friend key
-    file = open(path,'r',encoding='utf-8')                 #open file with friend key
-    friend_key = file.read()                               #read file
-    f = Fernet(friend_key)                                 #use fernet
+#     print('Please set the path to friend key file : ')
+#     
+#     path = input()                                         #user input for path to friend key
+#     file = open(path,'r',encoding='utf-8')                 #open file with friend key
+#     friend_key = file.read()                               #read file
+    f = Fernet(generate_sec_key())                                 #use fernet
     
     print('Please insert message of your friend below :')
 
-    decrypted_token = f.decrypt(userMesgToBytes(input()))  #decript message
+    decrypted_token = f.decrypt(userMesgToBytes(input()))  #decrypt message
     converted_to_unicode = decrypted_token.decode("utf-8") #decode kirilic symbols
     
-    print(converted_to_unicode)
+    print('\n',converted_to_unicode)
     
 #-----------------------
 #This method will convert string to bytes
@@ -104,9 +120,13 @@ def userMesgToBytes(user_message):
     convert = bytes(user_message, 'utf-8')                 #convert string to bytes
     
     return convert
+# 
+# def decodeToBase64(value):
+#     
+#     return base64.b64decode(value)
 
 def Main():
-    parse_args()                                           #parse arguments from command line 
+    parse_args()                                          #parse arguments from command line 
     
 if __name__=='__main__':
     Main()
